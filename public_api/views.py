dcce5cf6abe3738d6ee26fef58a140265cc43b62
vinoth <kmvinoth@gmail.com>
2018-12-07 14:54:14 +0100

    add uri_redirect view for the public api redirect url

--- public_api/views.py
+++ public_api/views.py
@@ -7,3 +7,22 @@ def home(request):
     home view render's the default Homepage template
     """
     return render(request, 'public_api/home.html')
+
+
+# Create your views here.
+def uri_redirect(request):
+    """
+    Redirect uri view
+    """
+    errors = []
+    if 'code' in request.GET:
+        auth_code = request.GET['code']
+        if not auth_code:
+            errors.append("Authorization code not present in request.GET dictionary")
+        elif len(auth_code) > 6 or len(auth_code) < 6:
+            errors.append("Length of the Authorization code is either greater than or less than 6")
+        else:
+            print("Authorization code = ", auth_code)
+            return render(request, 'public_api/redirect.html', {'authorization_code': auth_code})
+    else:
+        return render(request, 'public_api/redirect.html', {'errors': errors})
